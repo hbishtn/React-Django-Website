8553f483710ba0961fbb33b88b9fb805f4881703
hbishtn <hbishtn@gmail.com>
2026-08-24 22:53:07 +0530

    some changes in backend on models.py

--- backend/products/models.py
+++ backend/products/models.py
@@ -23,6 +23,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+    color_group = models.CharField(max_length=100, blank=True, null=True)
+    color_name = models.CharField(max_length=50, blank=True, null=True)
+    color_hex = models.CharField(max_length=7, blank=True, null=True)
 
     def __str__(self):
         return self.name
