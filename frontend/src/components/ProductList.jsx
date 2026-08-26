bace3dc10533dcae5e3ae01338f9fc6763ed2fa5
hbishtn <hbishtn@gmail.com>
2026-08-26 11:10:21 +0530

    fix pagination problem

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -48,7 +48,7 @@ function ProductList() {
 
     fetch(`${import.meta.env.VITE_API_URL}/categories/`)
       .then((response) => response.json())
-      .then((data) => setCategories(data));
+      .then((data) => setCategories(data.results || data));
   }, []);
 
   useEffect(() => {
