b19704f24fc267a73f778341431bd996e91a48c6
hbishtn <hbishtn@gmail.com>
2026-09-01 18:24:19 +0530

    Restart a new database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
         conn_max_age=600,
     )
 }
